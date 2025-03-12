b1f6ef20251957cec7c7d9c3a436fa8df91e5b1e
NilaAyu1611 <nilaayucw@gmail.com>
2025-03-12 20:14:02 +0700

    test

---
+++ android/app/src/main/kotlin/nila/id/test_paml1/MainActivity.kt
@@ -0,0 +1,5 @@
+package nila.id.test_paml1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(     // child paling tinggi
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("HomePage")),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 70, right: 70),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Budiono Siregar",style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.purple),),
+          Text("Flutter Development",style: TextStyle(color: Color.fromARGB(10, 186, 186, 5),),),
+          Row(children: [
+        OutlinedButton(onPressed: () {}, child: Text("Click Me")),
+        FilledButton(onPressed: () {}, child: Text("Click Me")),
+      ],
+      )],),
+      )
+
+      );
+  }
+}
